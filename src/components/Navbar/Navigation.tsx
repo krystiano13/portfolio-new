abf916b2aac8e182480bcd7bd71c8fb281725f91
krystiano13 <crystianotv@gmail.com>
2023-11-04 13:15:18 +0100

    create open and close animations for hamburger button

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navigation.css';
 
 const Navigation = () => {
+    const [open, setOpen] = useState<boolean>(false);
+
+    const openMenu = () => {
+        setOpen(prevState => {
+            return !prevState;
+        })
+    }
+
     return (
         <>
             <div className="NavWrap">
-                <div className="HamburgerButton">
-                    <div className="stripe"></div>
-                    <div className="stripe"></div>
-                    <div className="stripe"></div>
+                <div onClick={openMenu} className={open ? "HamburgerButton opened" : "HamburgerButton"}>
+                    <div id="s1" className={open ? "stripe upStripe" : "stripe"}></div>
+                    <div id="s2" className={open ? "stripe hidden" : "stripe"}></div>
+                    <div id="s3" className={open ? "stripe downStripe" : "stripe"}></div>
                 </div>
             </div>
             <nav></nav>
