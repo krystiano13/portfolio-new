d0036548ef48b8464167ea8cec45b1bdb00b5cf8
krystiano13 <crystianotv@gmail.com>
2023-11-04 13:02:21 +0100

    create styles of hamburger menu button

--- src/components/Navbar/Navigation.tsx
+++ src/components/Navbar/Navigation.tsx
@@ -4,10 +4,12 @@ import './Navigation.css';
 const Navigation = () => {
     return (
         <>
-            <div className="HamburgerButton">
-                <div className="stripe"></div>
-                <div className="stripe"></div>
-                <div className="stripe"></div>
+            <div className="NavWrap">
+                <div className="HamburgerButton">
+                    <div className="stripe"></div>
+                    <div className="stripe"></div>
+                    <div className="stripe"></div>
+                </div>
             </div>
             <nav></nav>
         </>
