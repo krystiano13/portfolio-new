a7e6cca280e566fdea6ad3a04585533032efece4
krystiano13 <crystianotv@gmail.com>
2023-11-11 10:59:30 +0100

    create styles for images in projects page

--- src/components/Projects/ProjectsData.ts
+++ src/components/Projects/ProjectsData.ts
@@ -1,9 +1,9 @@
-import custom from '../../images/custom.png';
+import custom from '../../../public/contactIcons/github.png'
 
 type project = {
     id : number,
     name : string,
-    images : ImageMetadata[],
+    images : string[],
     description: string,
     codeLink : string,
     liveLink : string
@@ -12,9 +12,36 @@ export const ProjectsData: project[] = [
     {
         id : 0,
         name : "Project 1",
-        images : [custom],
+        images : [custom.src],
         description : "Lorem Ipsum cos tam",
         codeLink : "",
         liveLink : ""
-    }
+    },
+
+    {
+        id : 1,
+        name : "Project 1",
+        images : [custom.src],
+        description : "Lorem Ipsum cos tam",
+        codeLink : "",
+        liveLink : ""
+    },
+
+    {
+        id : 2,
+        name : "Project 1",
+        images : [custom.src],
+        description : "Lorem Ipsum cos tam",
+        codeLink : "",
+        liveLink : ""
+    },
+
+    {
+        id : 3,
+        name : "Project 1",
+        images : [custom.src],
+        description : "Lorem Ipsum cos tam",
+        codeLink : "",
+        liveLink : ""
+    },
 ];
