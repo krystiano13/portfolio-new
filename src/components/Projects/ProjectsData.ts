1f0cd9dad0bb6b1110e6ec23563c29a9a5f5bbbe
krystiano13 <crystianotv@gmail.com>
2023-11-12 10:05:37 +0100

    add data about first project

--- src/components/Projects/ProjectsData.ts
+++ src/components/Projects/ProjectsData.ts
@@ -1,5 +1,8 @@
 import custom from '../../../public/contactIcons/github.png'
 
+import p1 from '../../../public/screenshots/p1.png';
+import p2 from '../../../public/screenshots/p2.png';
+
 type project = {
     id : number,
     name : string,
@@ -11,11 +14,15 @@ type project = {
 export const ProjectsData: project[] = [
     {
         id : 0,
-        name : "Project 1",
-        images : [custom.src],
-        description : "Lorem Ipsum cos tam",
-        codeLink : "",
-        liveLink : ""
+        name : "GiardDesign",
+        images : [p1.src, p2.src],
+        description : "This is a landing page that was created for the purposes of recruitment for the company\n" +
+            "AdRespect. The project includes a fully functional slider, a photo gallery, and smooth animations of\n" +
+            "its elements during scrolling. To maximize the project's performance, I rewrote it later using the\n" +
+            "Astro framework. In addition to the mentioned technology, I also used Bootstrap, SCSS, AOS.js, and\n" +
+            "Macy.js.",
+        codeLink : "https://github.com/krystiano13/astro_rtask",
+        liveLink : "https://krystiano13.github.io/astro_rtask/"
     },
 
     {
